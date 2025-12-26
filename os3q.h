9cf274645bbea5cdbb97e00d33049786b819e1ab
adi0258 <adita3@mta.ac.il>
2025-12-26 11:31:21 +0200

    minor changes to h file

--- os3q.h
+++ os3q.h
@@ -1,26 +1,13 @@
 #ifndef OS3Q_H
 #define OS3Q_H
 
-// The list itself is defined as a struct named QueueOS
 typedef struct QueueOS QueueOS;
 
-
-// Init: Receives max size and initializes a new queue
 QueueOS* init(int max_size);
-
-// Destroy: Deletes queue and frees allocations
 void destroy(QueueOS* q);
-
-// Enqueue: Receives a value (long) and adds it to the end
 void enqueue(QueueOS* q, long value);
-
-// Dequeue: Removes element from head and returns its value
 long dequeue(QueueOS* q);
-
-// Size: Returns the number of elements
 int size(QueueOS* q);
-
-// Sum: Returns the sum of all elements
 long sum(QueueOS* q);
 
 #endif
